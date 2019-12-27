1fb026909b40e8757251799466200f3e649fdb74
Simon Berhanu <simon1515@hotmail.com>
2019-12-27 00:53:54 -0500

    Map state and dispatch to props

--- src/components/cake-container.jsx
+++ src/components/cake-container.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux';
 
-export function CakeContainer() {
+export function CakeContainer(props) {
     return (
         <div>
-            <h2>Number of cakes</h2>
-            <button>Buy Cake</button>
+            <h2>Number of cakes - {props.nCakes} </h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     );
 };
+
+// Selectors (usually in a seperate file)
+const mapStateToProps = state => { nCakes: state.nCakes };
+
+const mapDispatchToProps = dispatch => { buyCake: () => dispatch(buyCake()) }
+
+export function connect(mapStateToProps, mapDispatchToProps)
+(CakeContainer);
---
+++ src/redux/index.js
@@ -0,0 +1,1 @@
+import { buyCake } from './cake/cakeActions';
