4661927241d2dccca637edf565c18ad3c147fbee
Simon Berhanu <simon1515@hotmail.com>
2019-12-28 00:09:07 -0500

    Add ItemContainer (OwnProps)

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import CakeContainer from './components/CakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer />
         <CakeContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
