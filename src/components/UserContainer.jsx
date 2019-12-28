b08435f9fa779defbea0baa54120c8d0c16cdc77
Simon Berhanu <simon1515@hotmail.com>
2019-12-28 02:31:16 -0500

    Add fetch users and useEffect

--- src/components/UserContainer.jsx
+++ src/components/UserContainer.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchUsers } from '../redux';
+import { connect } from 'react-redux';
 
-function UserContainer() {
+function UserContainer({ userData, fetchUsers }) {
+    useEffect(() => { fetchUsers() }, []);
     return (
-        <div>
-            Users
-        </div>
+        userData.isLoading ?
+            (<h2>Loading...</h2>)
+            : userData.error ?
+                (<h2>{userData.error}</h2>)
+                : (
+                    <div>
+                        <h2>User List</h2>
+                        <div>{userData && userData.users
+                            && userData.users.map(
+                                user => <p>{user.name}</p>
+                            )}</div>
+                    </div>
+                )
     )
 }
 
-export default UserContainer
+const mapStateToProps = state => {
+    return {
+        userData: state.user
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchUsers: () => dispatch(fetchUsers())
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer)
--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,6 +1,7 @@
 import {
     FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE
 } from "./userTypes";
+import axios from "axios";
 
 export const fetchUsersRequest = () => {
     return {
@@ -21,3 +22,19 @@ export const fetchUsersFailure = error => {
         payload: error
     };
 };
+
+// Uses thunk middleware
+export const fetchUsers = () => {
+    return dispatch => {
+        dispatch(fetchUsersRequest);
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                const users = response.data;
+                dispatch(fetchUsersSuccess(users));
+            })
+            .catch(error => {
+                const errorMsg = error.message;
+                dispatch(fetchUsersFailure(errorMsg));
+            });
+    }
+}
