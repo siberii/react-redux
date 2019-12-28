1450ae2a9bcc8ec4eef2bf2c5d892df0b2f13603
Simon Berhanu <simon1515@hotmail.com>
2019-12-27 23:07:26 -0500

    Add redux devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from "redux-logger";
 import { rootReducer } from "./rootReducer";
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger))
+);
